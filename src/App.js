3d7c4eb35315ffcebbf9c4d205ba07975573876c
LUCAS DOS SANTOS RODRIGUES DA SILVA <LUCAS.SILVA@hnl.local>
2022-11-04 18:00:40 -0300

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-
 function App() {
+  
+  const name ="Lucas"
+  const newName = name.toLocaleUpperCase()
+
+  function sum(a,b)
+  {
+    return a + b
+  }
+  const url = "https://via.placeholder.com/150*"
+
   return (
     <div className="App">
-<h1>Ola react</h1>
-<p>Meu primeiro app</p>
-
+<h1>Alternando o JSX</h1>
+<p>Olá, {newName}</p>
+<p> Soma: {sum(1,2)} </p>
+<img src={url} alt="minha imagem"></img>
     </div>
   );
 }
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,6 @@
+function HelloWorld()
+{
+
+}
+
+export default HelloWorld
