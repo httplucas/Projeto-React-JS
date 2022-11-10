66a679adb3422d2fde6faa31c1a3486587344161
Lucas <lucasdossantosrs321@gmail.com>
2022-11-10 19:50:51 -0300

    React-Fragments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
+import Frase from './components/Frase'
+import List from './components/List'
 function App() {
-  
-  const name ="Lucas"
-  const newName = name.toLocaleUpperCase()
 
-  function sum(a,b)
-  {
-    return a + b
-  }
-  const url = "https://via.placeholder.com/150*"
-
+  const nome = "Vania"
   return (
     <div className="App">
-<h1>Alternando o JSX</h1>
-<p>Olá, {newName}</p>
-<p> Soma: {sum(1,2)} </p>
-<img src={url} alt="minha imagem"></img>
+      <Frase/>
+      <Frase/>
+  <SayMyName nome="Lucas"/>
+  <SayMyName nome="Joao"/>
+  <SayMyName nome={nome}/>
+  <Pessoa nome="Lucas" idade="18" profissao="Dev" foto="https://via.placeholder.com/150" />
+  <List/>
     </div>
   );
 }
